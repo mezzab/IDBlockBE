d2b13a3a65a0d14c79782819b50943ef1f4f4593
mezzab <marcos32m@gmail.com>
2018-07-25 00:01:07 -0300

    manda mails

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,25 +1,38 @@
+var nodemailer = require("nodemailer");
 module.exports = function(app, db) {
-  const collection = app.post("/saveUserInSC", (req, res) => {
-    // const note = { text: req.body.body, title: req.body.title };
-    // db.collection('notes').insert(note, (err, result) => {
-    //   if (err) {
-    //     res.send({ 'error': 'An error has occurred' });
-    //   } else {
-    //     res.send(result.ops[0]);
-    //   }
-    // });
-    res.send("no implementamos esto todavia guacho");
-  });
-
   app.post("/sendEmail", (req, res) => {
-    console.log(req.body);
+    //todo: generar un codigo automaticamente
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "idblocktest@gmail.com",
+        pass: "marcoszxc"
+      }
+    });
 
-    // We will send an email here
+    var mailOptions = {
+      from: "idblocktest@gmail.com",
+      to: req.body.mail,
+      subject: "Tu código de verificación de IDBlock",
+      html:
+        "<p> Tu codigo de verificacion es: <b>123123</b> <p>Este código caducará en 10 minutos.  </p> </p>"
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent to" + req.body.mail + info.response);
+      }
+    });
 
     res.send({ code: "123123" });
-    // example db
-    //   const note = { text: req.body.body, title: req.body.title}
-    //   db.collection('notes').insert(note, (err, results) => {
-    // }
+    /* lo ideal seria guardar el codigo en una db con el mail
+      y cuando el user presiona continuar abria que pegarle de nuevo al back con el codigo
+      para que el verifique si es el que guardo en la db.*/
+  });
+
+  app.post("/saveUserInSmartContract", (req, res) => {
+    res.send("no implementamos esto todavia guacho");
   });
 };
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 const app = express();
+const nodemailer = require("nodemailer");
 
 const port = 8000;
-
+app.use(bodyParser.urlencoded({ extended: true }));
 require("./app/routes")(app, {});
 app.listen(port, () => {
   console.log("We are live on " + port);
