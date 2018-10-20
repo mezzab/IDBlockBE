e4243e59b7657ecb46825d05d1f5d1a2c718439d
Tiincho94 <martinbec1@hotmail.com>
2018-10-20 14:29:16 -0300

    Connect Ganache Backend

--- server.js
+++ server.js
@@ -4,12 +4,27 @@ const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 const app = express();
 const nodemailer = require("nodemailer");
+const Web3 = require("web3");
 
 const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 require("./app/routes")(app, {});
 app.listen(port, () => {
-  console.log("We are live on " + port);
+console.log("We are live on " + port);
+
+ 	//if (typeof web3 !== 'undefined') {
+  	//web3 = new Web3(web3.currentProvider);
+	//} else {
+  	//web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+	//}
+
+var Eth = require('web3-eth');
+var eth = new Eth(Eth.givenProvider || "http://localhost:8545");
+var Web3 = require('web3');
+var web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+
+console.log("Las cuentas ganache son:");
+web3.eth.getAccounts(console.log);
 });
 
 // MongoClient.connect(
