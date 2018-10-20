52dea873eb609fab45640d39c70081be5258fbbd
Franco Scagnetti <franscagnetti@hotmail.com>
2018-10-20 22:00:54 +0000

    compile hello.sol contract

--- server.js
+++ server.js
@@ -5,12 +5,26 @@ const bodyParser = require("body-parser");
 const app = express();
 const nodemailer = require("nodemailer");
 const Web3 = require("web3");
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
 
 const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 require("./app/routes")(app, {});
 app.listen(port, () => {
 console.log("We are live on " + port);
+
+
+
+const helloPath = path.resolve(__dirname, 'contracts', 'hello.sol');
+const source = fs.readFileSync(helloPath, 'UTF-8');
+
+console.log(solc.compile(source,1))
+module.exports = solc.compile(source, 1).contracts[':Hello'];
+
+
+
 
  	if (typeof web3 !== 'undefined') {
   	web3 = new Web3(web3.currentProvider);
