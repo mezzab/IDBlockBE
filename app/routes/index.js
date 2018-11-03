70d2ae1ac6587dfc3be1869796cc895eb710c01d
mezzab <marcos32m@gmail.com>
2018-11-03 17:38:07 -0300

    integrate ipfs with smart contract

--- app/routes/index.js
+++
@@ -1,5 +0,0 @@
-const noteRoutes = require ('./routes');
-module.exports = function (app, db) {
-  noteRoutes (app, db);
-  // Other route groups could go here, in the future
-};
--- server.js
+++ server.js
@@ -1,45 +1,117 @@
-// server.js
-const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const bodyParser = require("body-parser");
-const app = express();
-const nodemailer = require("nodemailer");
-const Web3 = require("web3");
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const Web3 = require('web3');
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
+const ipfsAPI = require('ipfs-api');
+var develop = require('./config/develop');
+var utils = require('./utils');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 8000;
-app.use(bodyParser.urlencoded({ extended: true }));
-require("./app/routes")(app, {});
+
 app.listen(port, () => {
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * *    INIT   * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+    console.log('✓ Backend escuchando en puerto ' + port + '\n');
 
-    console.log("✓ Backend escuchando en puerto " + port);
-    console.log(' ')
-    console.log(' ')
-    if (typeof web3 !== 'undefined') {
-        web3 = new Web3(web3.currentProvider);
-    } else {
-        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    }
-    console.log("✓ Conectado a ganache!");
-
-    web3.eth.getAccounts(function (error, accounts) {
-        console.log(" > Las cuentas ganache son: ");
-        accounts.map((x, i) => console.log('  ' + i + ') ' + x))
-        console.log(" > La cuenta a utilizar sera: ");
-        console.log('  0)' + accounts[0])
-        web3.eth.defaultAccount = accounts[0];
-        console.log(' ')
-        console.log(' ')
-    })
-
+    connectToBlockchain();
     startSmartContract();
 
-    // Blockchain Routes - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * *    ROUTES   * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+    app.post('/sendEmail', (req, res) => {
+        console.log('* * * * * *  EMAIL * * * * * * ');
+        var transporter = nodemailer.createTransport({
+            service: develop.mail.service,
+            auth: {
+                user: develop.mail.user,
+                pass: develop.mail.pass,
+            },
+        });
+
+        var code = utils.randomCode();
+
+        transporter.sendMail(utils.mailOptions(req.body.mail, code), function (
+            error,
+            info
+        ) {
+            if (error) {
+                console.log('Error sending mail: ', error);
+            } else {
+                console.log(
+                    'Email sent to: ' +
+                    req.body.mail +
+                    '. Verification code: *** ' +
+                    code +
+                    ' *** .Extra info:' +
+                    info.response
+                );
+            }
+        });
+        res.send({ code });
+    });
+
+    app.post('/sendSMS', (req, res) => {
+        console.log('* * * * * *  SMS * * * * * * ');
+        var accountSid = develop.sms.accountSid; // Your Account SID from www.twilio.com/console
+        var authToken = develop.sms.authToken; // Your Auth Token from www.twilio.com/console
+
+        var twilio = require('twilio');
+        var client = new twilio(accountSid, authToken);
+
+        var code = utils.randomCode();
+        console.log('SMS sent. ' + '. Verification code: *** ' + code + ' *** .');
+        client.messages
+            .create({
+                body: 'Yor code is: *** ' + code,
+                to: '+54' + req.body.phone, // Text this number
+                from: '+15866661838', // From a valid Twilio number
+            })
+            .then(message => console.log('message.sid: ', message.sid));
+        res.send({ code });
+    });
+
+    app.post('/saveBlock', (req, res) => {
+        console.log('> SAVE USER BLOCK IN IPFS');
+
+        // Aca se realizara el chequeo para validar el legajo.
+
+        const dni = JSON.parse(req.body.legajo).dni;
+        const addr = req.body.entityAddr;
+        const entityAddress = addr ? addr : accounts[5];
+        console.log('\n' + '> El legajo que se guardara es: ' + '\n' + req.body.legajo);
+        console.log('\n' + '> El addres de la entidad es: ' + '\n' + entityAddress)
+
+        const ipfs = ipfsAPI('ipfs.infura.io', '5001', { protocol: 'https' });
+        let testBuffer = new Buffer(req.body.legajo);
+
+        return ipfs.files.add(testBuffer, function (err, file) {
+            if (err) {
+                console.log('Error saving file: ', err);
+                res.send(err)
+            }
+            const hash = file[0].hash;
+            console.log('\n' + '✓ File successfully saved in IPFS.');
+            console.log('\n' + '> El hash es: ' + '\n' + hash)
+
+            if (!dni || !hash) {
+                const msg = 'Es necesario un dni y un hash para ejecutar la funcion del Smart Contract.'
+                console.log('\n' + msg);
+                return res.send(msg)
+            }
+
+            setHash(dni, hash);
+
+            setRelation(dni, entityAddress);
+
+            res.send({ code: 200, msg: 'Se guardo correctamente el hash en el smart contract' });
+        });
+    });
 
     app.post('/initSmartContract', (req, res) => {
-
         startSmartContract()
             .then(() => {
                 res.send('EXITO');
@@ -56,11 +128,11 @@ app.listen(port, () => {
         if (!dni) return res.send('Es necesario un dni para realizar la consulta.')
         getHash(dni, entityAddress)
             .then((hash) => {
-                console.log("✓ Exito! El hash es: ", hash);
+                console.log('✓ Exito! El hash es: ', hash);
                 res.send(hash);
             })
             .catch(err => {
-                console.log("Error: No es posible acceder al hash.");
+                console.log('Error: No es posible acceder al hash.');
                 res.send({ msg: 'Error: No es posible acceder al hash.', err })
             });
     });
@@ -99,16 +171,33 @@ app.listen(port, () => {
 
 });
 
+
+/* * * * * * * * * * * * * * * * * * * * *   SMART CONTRACT UTILS   * * * * * * * * * * * * * * * * * * * * * */
 let abi;
 let bytecode;
 let contractInstance;
 let accounts;
 
+function connectToBlockchain() {
+    if (typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+    }
+    console.log('✓ Conectado a ganache!');
+    web3.eth.getAccounts(function (error, accounts) {
+        console.log(' > Las cuentas ganache son: ');
+        accounts.map((x, i) => console.log('  ' + i + ') ' + x))
+        console.log('\n' + ' > La cuenta a utilizar sera: ');
+        console.log('  0)' + accounts[0] + '\n' + '\n')
+        web3.eth.defaultAccount = accounts[0];
+    })
+}
+
 async function startSmartContract() {
-    console.log(' ')
-    console.log('> Building and deploying smart contract... ');
+    console.log('\n' + '> Building and deploying smart contract... ' + '\n');
     accounts = await web3.eth.getAccounts();
-    console.log("   Buscando smart contract IdentitiesBlock.sol...");
+    console.log('   Buscando smart contract IdentitiesBlock.sol...' + '\n');
 
     let helloPath = path.resolve(__dirname, 'contracts', 'IdentitiesBlock.sol');
     let code = fs.readFileSync(helloPath, 'UTF-8').toString();
@@ -118,7 +207,6 @@ async function startSmartContract() {
 
     let identitiesContract = new web3.eth.Contract(abi, { from: accounts[0], gas: 47000, data: bytecode });
 
-    console.log("   Deployando y deployando smart contrat...");
     contractInstance = await identitiesContract.deploy()
         .send({
             from: accounts[0],
@@ -139,35 +227,46 @@ async function startSmartContract() {
 
     // console.log('contractInstance.options:', contractInstance.options);
 
-    console.log("");
-    console.log("✓ Completado!");
+    console.log('\n' + '✓ Se deployo correctamente el smart contract!' + '\n');
 }
 
 // Recibe un dni y un address de la entidad que quiere conocer el hash.
 async function getHash(dni, entityAddr) {
-    console.log(' ')
-    console.log(' ')
-    console.log('La entidad de address: ', entityAddr)
+    console.log('\n' + '\n' + 'La entidad de address: ', entityAddr)
     console.log('Busca el hash de ' + dni + '...')
     return await contractInstance.methods.getHash(dni).call({ from: entityAddr });
 }
 
 // Recibe el dni del usuario y el hash a IPFS donde esta almacenado el legajo con la informacion. 
 async function setHash(dni, hash) {
-    console.log(' ')
-    console.log('> SetHash ')
+    console.log('\n' + '> SET HASH IN SMART CONTRACT ')
     console.log('Guardando el hash:')
     console.log('   Dni:', dni)
     console.log('   Hash:', hash)
-    return await contractInstance.methods.setHash(dni, hash).send({ from: accounts[0], gas: 150000 });
+    // accounts[0] es nuestro addres (con el que creamos el smartContract);
+    try {
+        const transaction = await contractInstance.methods.setHash(dni, hash).send({ from: accounts[0], gas: 150000 });
+        console.log('\n' + '✓ Exito! Se guardo correctamente el hash en el SC.');
+        console.log('Transaction information: ');
+        console.log(transaction);
+    } catch (err) {
+        res.send({ msg: 'Error: No es posible guardar el hash en el contrato.', err });
+    }
 }
 
 // Recibe el dni del usuario y el address de la entidad para generar la relacion en la whitelist
 async function setRelation(dni, entityAddr) {
     console.log(' ')
-    console.log('> SetRelation ')
+    console.log('> SET RELATION IN SMART CONTRACT ')
     console.log('Guardando relacion:')
     console.log('   Dni:', dni)
     console.log('   Cuenta:', entityAddr)
-    return await contractInstance.methods.setRelation(dni, entityAddr).send({ from: accounts[0], gas: 150000 });
+    try {
+        const transaction = await contractInstance.methods.setRelation(dni, entityAddr).send({ from: accounts[0], gas: 150000 });
+        console.log('\n' + '✓ Exito! Se guardo correctamente la relacion entidad/dni en el SC.');
+        console.log('Transaction information: ');
+        console.log(transaction);
+    } catch (err) {
+        res.send({ msg: 'Error: No es posible crear la relacion dni/entityAddress.', err });
+    }
 }
