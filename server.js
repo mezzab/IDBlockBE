1d3a3c15c12734015bb118fa8fe2a724003f5e99
Tiincho94 <martinbec1@hotmail.com>
2018-11-07 21:20:03 -0300

    Save image base 64

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var develop = require('./config/develop');
 var utils = require('./utils');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 const port = 8000;
 
